69a6457496caabb2669bc7d0cc3626e57a54eda7
nassir <dullykaka001@gmail.com>
2023-06-30 17:19:14 +0300

    initial commit

---
+++ src/Course.java
@@ -0,0 +1,21 @@
+public class Course {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        //String letter = sc.nextLine().toLowerCase();
+
+//        System.out.println(demo.add(2, 5));
+
+      Demo.numb=100;
+      Demo b1=new Demo();
+      Demo b2= new Demo();
+
+      Demo.numb=200;
+
+        System.out.println(b1.numb);
+        System.out.println(b2.numb);
+//        demo.type="suzuki";
+//        demo.model="sample 2";
+//        demo.sample();
+
+    }
+    }
---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo{
+
+    static int numb=0;
+
+    Demo(){
+        numb+=1;
+    }
+}
